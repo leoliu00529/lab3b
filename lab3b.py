711b2415bec882fc48c96dae815a834dad40e4ef
Yuan Shen <classxib@lnxsrv09.seas.ucla.edu>
2020-03-08 01:19:57 -0800

    dlafjl

--- lab3b.py
+++ lab3b.py
@@ -21,7 +21,7 @@ free_list = defaultdict(int)
 reference_list = defaultdict(int)
 free_inode_list = defaultdict(int)
 inode_reference_list = defaultdict(int)
-
+dir_counts = defaultdict(int)
 
 with open(file_path, "r") as f:
     reader = csv.reader(f, delimiter=',')
@@ -45,6 +45,8 @@ with open(file_path, "r") as f:
             reference_list[row[5]] += 1
         if row[0] == 'IFREE':
             free_inode_list[row[1]] += 1
+        if row[0] == 'DIRENT':
+            dir_counts[row[3]] += 1
 
 with open(file_path, "r") as f:
     reader = csv.reader(f, delimiter=',')
@@ -62,6 +64,7 @@ with open(file_path, "r") as f:
 
     for row in reader:
         if row[0] == 'INODE':
+
             for i, each_block in enumerate(row[12:24]):
                 if int(each_block) < 0 or int(each_block) > num_block:
                     print(f"INVALID BLOCK {each_block} IN INODE {row[1]} AT OFFSET {i}")
@@ -81,13 +84,13 @@ with open(file_path, "r") as f:
                             print(f"ALLOCATED BLOCK {each_block} ON FREELIST")
                     elif reference_list[each_block] > 1:
                         if i == 13:
-                            print(f"DUPLICATE INDIRECT BLOCK {each_block}")
+                            print(f"DUPLICATE INDIRECT BLOCK {each_block} IN INODE {row[1]} AT OFFSET 12")
                         elif i == 14:
-                            print(f"DUPLICATE DOUBLE INDIRECT BLOCK {each_block}")
+                            print(f"DUPLICATE DOUBLE INDIRECT BLOCK {each_block} IN INODE {row[1]} AT OFFSET 268")
                         elif i == 15:
-                            print(f"DUPLICATE TRIPLE INDIRECT BLOCK {each_block}")
+                            print(f"DUPLICATE TRIPLE INDIRECT BLOCK {each_block} IN INODE {row[1]} AT OFFSET 65804")
                         else:
-                            print(f"DUPLICATE BLOCK {each_block}")
+                            print(f"DUPLICATE BLOCK {each_block} IN INODE {row[1]} AT OFFSET {i}")
 
             if int(row[24]) < 0 or int(row[24]) > num_block:
                 print(f"INVALID INDIRECT BLOCK {row[24]} IN INODE {row[1]} AT OFFSET 12")
@@ -102,6 +105,8 @@ with open(file_path, "r") as f:
             if int(row[26]) > 0 and int(row[26]) < reserved:
                 print(f"RESERVED TRIPLE INDIRECT BLOCK {row[26]} IN NODE {row[1]} AT OFFSET 65804")
 
+            if dir_counts[row[1]] != int(row[6]):
+                print(f"INODE {row[1]} HAS {dir_counts[row[1]]} LINKS BUT LINKCOUNT IS {row[6]}")
 
 
         if row[0] == 'INDIRECT':
@@ -114,4 +119,5 @@ with open(file_path, "r") as f:
                 print(f"INVALID {level} BLOCK {row[5]} IN INODE {row[1]} AT OFFSET {row[3]}")
             if int(row[5]) > 0 and int(row[5]) < reserved:
                 print(f"RESERVED {level} BLOCK {row[5]} IN INODE {row[1]} AT OFFSET {row[3]}")
-
+            if reference_list[row[5]] > 1:
+                print(f"DUPLICATE {level} BLOCK {row[5]} IN INODE {row[1]} AT OFFSET {row[3]}")
