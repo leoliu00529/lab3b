5c1bd056cd2c5b20cb1249f8a8a4935b643e9d64
Yuan Shen <classxib@lnxsrv09.seas.ucla.edu>
2020-03-07 23:17:48 -0800

    ??

--- lab3b.py
+++ lab3b.py
@@ -29,25 +29,26 @@ with open(file_path, "r") as f:
 
 with open(file_path, "r") as f:
     reader = csv.reader(f, delimiter=',')
+    reserved = 4 + num_inode / 8.0
     for row in reader:
         if row[0] == 'INODE':
             for i, each_block in enumerate(row[12:24]):
                 if int(each_block) < 0 or int(each_block) > num_block:
                     print(f"INVALID BLOCK {each_block} IN INODE {row[1]} AT OFFSET {i}")
-                if int(each_block) > 0 and int(each_block) < first_non_reserved_inode:    
+                if int(each_block) > 0 and int(each_block) < reserved:    
                     print(f"RESERVED BLOCK {each_block} IN INODE {row[1]} AT OFFSET {i}")
-            if int(row[13]) < 0 or int(row[13]) > num_block:
-                print(f"INVALID BLOCK {row[13]} IN INODE {row[1]} AT OFFSET 12")
-            if int(row[13]) > 0 and int(row[13]) < first_non_reserved_inode:
-                print(f"RESERVED BLOCK {row[13]} IN NODE {row[1]} AT OFFSET 12")
-            if int(row[14]) < 0 or int(row[14])> num_block:
-                print(f"INVALID BLOCK {row[14]} IN INODE {row[1]} AT OFFSET 268")
-            if int(row[13]) > 0 and int(row[14]) < first_non_reserved_inode:
-                print(f"RESERVED BLOCK {row[14]} IN NODE {row[1]} AT OFFSET 268")
-            if int(row[15]) < 0 or int(row[15])> num_block:
-                print(f"INVALID BLOCK {row[15]} IN INODE {row[1]} AT OFFSET 65804")
-            if int(row[15]) > 0 and int(row[15]) < first_non_reserved_inode:
-                print(f"RESERVED BLOCK {row[15]} IN NODE {row[1]} AT OFFSET 65804")
+            if int(row[24]) < 0 or int(row[24]) > num_block:
+                print(f"INVALID INDIRECT BLOCK {row[24]} IN INODE {row[1]} AT OFFSET 12")
+            if int(row[24]) > 0 and int(row[24]) < reserved:
+                print(f"RESERVED INDIRECT BLOCK {row[24]} IN NODE {row[1]} AT OFFSET 12")
+            if int(row[25]) < 0 or int(row[25])> num_block:
+                print(f"INVALID DOUBLE INDIRECT BLOCK {row[25]} IN INODE {row[1]} AT OFFSET 268")
+            if int(row[25]) > 0 and int(row[25]) < reserved:
+                print(f"RESERVED DOUBLE INDIRECT BLOCK {row[25]} IN NODE {row[1]} AT OFFSET 268")
+            if int(row[26]) < 0 or int(row[26])> num_block:
+                print(f"INVALID TRIPLE INDIRECT BLOCK {row[26]} IN INODE {row[1]} AT OFFSET 65804")
+            if int(row[26]) > 0 and int(row[26]) < reserved:
+                print(f"RESERVED TRIPLE INDIRECT BLOCK {row[26]} IN NODE {row[1]} AT OFFSET 65804")
 
 
 
